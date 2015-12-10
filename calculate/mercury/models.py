50aa14ab61d9e7285b273c89cf35dc922e57760a
Ylia <skripka696@ukr.net>
2015-12-10 14:11:26 +0200

    new models

--- calculate/mercury/models.py
+++ calculate/mercury/models.py
@@ -10,123 +10,123 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=80)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    group_id = models.IntegerField()
-    permission_id = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group_id', 'permission_id'),)
-
-
-class AuthMessage(models.Model):
-    user_id = models.IntegerField()
-    message = models.TextField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_message'
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=50)
-    content_type_id = models.IntegerField()
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type_id', 'codename'),)
-
-
-class AuthUser(models.Model):
-    username = models.CharField(unique=True, max_length=30)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=75)
-    password = models.CharField(max_length=128)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    is_superuser = models.IntegerField()
-    last_login = models.DateTimeField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    user_id = models.IntegerField()
-    group_id = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user_id', 'group_id'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user_id = models.IntegerField()
-    permission_id = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user_id', 'permission_id'),)
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    user_id = models.IntegerField()
-    content_type_id = models.IntegerField(blank=True, null=True)
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    name = models.CharField(max_length=100)
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
-
-class DjangoSite(models.Model):
-    domain = models.CharField(max_length=100)
-    name = models.CharField(max_length=50)
-
-    class Meta:
-        managed = False
-        db_table = 'django_site'
+# class AuthGroup(models.Model):
+#     name = models.CharField(unique=True, max_length=80)
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_group'
+#
+#
+# class AuthGroupPermissions(models.Model):
+#     group_id = models.IntegerField()
+#     permission_id = models.IntegerField()
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_group_permissions'
+#         unique_together = (('group_id', 'permission_id'),)
+#
+#
+# class AuthMessage(models.Model):
+#     user_id = models.IntegerField()
+#     message = models.TextField()
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_message'
+#
+#
+# class AuthPermission(models.Model):
+#     name = models.CharField(max_length=50)
+#     content_type_id = models.IntegerField()
+#     codename = models.CharField(max_length=100)
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_permission'
+#         unique_together = (('content_type_id', 'codename'),)
+#
+#
+# class AuthUser(models.Model):
+#     username = models.CharField(unique=True, max_length=30)
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#     email = models.CharField(max_length=75)
+#     password = models.CharField(max_length=128)
+#     is_staff = models.IntegerField()
+#     is_active = models.IntegerField()
+#     is_superuser = models.IntegerField()
+#     last_login = models.DateTimeField()
+#     date_joined = models.DateTimeField()
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_user'
+#
+#
+# class AuthUserGroups(models.Model):
+#     user_id = models.IntegerField()
+#     group_id = models.IntegerField()
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_user_groups'
+#         unique_together = (('user_id', 'group_id'),)
+#
+#
+# class AuthUserUserPermissions(models.Model):
+#     user_id = models.IntegerField()
+#     permission_id = models.IntegerField()
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'auth_user_user_permissions'
+#         unique_together = (('user_id', 'permission_id'),)
+#
+#
+# class DjangoAdminLog(models.Model):
+#     action_time = models.DateTimeField()
+#     user_id = models.IntegerField()
+#     content_type_id = models.IntegerField(blank=True, null=True)
+#     object_id = models.TextField(blank=True, null=True)
+#     object_repr = models.CharField(max_length=200)
+#     action_flag = models.SmallIntegerField()
+#     change_message = models.TextField()
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'django_admin_log'
+#
+#
+# class DjangoContentType(models.Model):
+#     name = models.CharField(max_length=100)
+#     app_label = models.CharField(max_length=100)
+#     model = models.CharField(max_length=100)
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'django_content_type'
+#         unique_together = (('app_label', 'model'),)
+#
+#
+# class DjangoSession(models.Model):
+#     session_key = models.CharField(primary_key=True, max_length=40)
+#     session_data = models.TextField()
+#     expire_date = models.DateTimeField()
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'django_session'
+#
+#
+# class DjangoSite(models.Model):
+#     domain = models.CharField(max_length=100)
+#     name = models.CharField(max_length=50)
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'django_site'
 
 
 class MercuryAccountdetails(models.Model):
