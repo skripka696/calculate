db003fefce657dd9dcb3e47a923a183bba95222b
TurboBelka <turbo.belka@bk.ru>
2015-12-14 10:17:45 +0200

    Add certifictaion, location serializers

--- calculate/mercury/serializers.py
+++ calculate/mercury/serializers.py
@@ -13,4 +13,25 @@ class CurrencySerializer(serializers.ModelSerializer):
 class AssociationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Agentassociations
-        fields = ('id', 'name')
+
+
+class CertificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agentcertifications
+        
+
+class LocationSerializer(serializers.ModelSerializer):
+    Ports = serializers.SerializerMethodField()
+    symbol = serializers.SerializerMethodField()
+
+    def get_Ports(self, obj):
+        return obj.ports_set.all().values_list('pk', flat=True)
+
+    def get_symbol(self, obj):
+        if obj:
+            return obj.name[:3].upper()
+        return None
+
+    class Meta:
+        model = Location
+        fields = ('Ports', 'id', 'name', 'symbol')
